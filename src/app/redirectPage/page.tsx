5283e6db0ba3db656bd5554a81fbb8b85be5ef99
godspower <godspjona@gmail.com>
2025-05-06 23:03:59 +0100

    git initialize

---
+++ src/app/redirectPage/page.tsx
@@ -0,0 +1,8 @@
+export default function RedirectPage() {
+    return (
+      <div style={{ padding: 20 }}>
+        <h1>This website is for mobile devices only</h1>
+        <p>Please view this site on your phone or tablet.</p>
+      </div>
+    );
+  }
