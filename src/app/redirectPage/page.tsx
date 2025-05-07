8933c03a237e232b16be40bbf5fd7b7709f65d6d
godspower <godspjona@gmail.com>
2025-05-07 09:58:41 +0100

    Merge remote-tracking branch 'origin/main' into main

--- src/app/redirectPage/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function RedirectPage() {
-    return (
-      <div style={{ padding: 20 }}>
-        <h1>This website is for mobile devices only</h1>
-        <p>Please view this site on your phone or tablet.</p>
-      </div>
-    );
-  }
