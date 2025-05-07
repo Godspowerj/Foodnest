672c19d3e40bd673824360485b058c16777ed198
godspower <godspjona@gmail.com>
2025-05-07 11:11:32 +0100

    in progress

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,14 @@
 export default function Home() {
   return (
     <div>
-      <p className="text-black">welcome to Errandgo</p>
+      <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-b from-black to-gray-900">
+        <h1 className="text-4xl font-bold text-center text-white ">
+          errandgo
+        </h1>
+        <p className="text-center text-gray-300 text-base">
+        Your solution for managing errands.        </p>
+      </div>
+      
     </div>
   );
 }
