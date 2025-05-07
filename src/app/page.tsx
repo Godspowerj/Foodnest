09e7d61e1f05ac3ad8afe494dc56b59e7c8ba8ac
godspower <godspjona@gmail.com>
2025-05-07 11:31:14 +0100

    in progress

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
 
-
-export default function Home() {
+const Home = () => {
   return (
-    <div>
+
       <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-b from-black to-gray-900">
-        <h1 className="text-4xl font-bold text-center text-white ">
+        <h1 className="text-3xl font-bold text-center text-white">
           errandgo
         </h1>
-        <p className="text-center text-gray-300 text-base">
-        Your solution for managing errands.        </p>
+        <p className="text-center text-gray-600 text-sm">
+          Your one-stop solution for managing errands and tasks.
+        </p>
       </div>
-      
-    </div>
+
   );
 }
+export default Home;
