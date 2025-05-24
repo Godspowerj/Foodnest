7e387ffc02f0291d0b0cb337eb3422a4d6a68609
godspower <godspjona@gmail.com>
2025-05-24 09:19:37 +0100

    in progress

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,12 +1,31 @@
-import React from 'react'
-import Input from './input'
+import React from "react";
+import Input from "./input";
+import Image from "next/image";
+import { hamburgerIcon as HamburgerIcon, ShoppingIcon } from "@/asset/asset";
 const Navbar = () => {
   return (
-    <div className='md:flex items-center  justify-between'>
-      <h2 className='text-2xl font-medium'>Choose category</h2>
+    <div className="md:flex items-center  justify-between">
+      <header className="lg:hidden flex items-center justify-between w-full pb-2">
+        <div className="size-7 ">
+          <HamburgerIcon />
+        </div>
+
+        <Image
+          src="/logo.png"
+          alt="Logo"
+          width={100}
+          height={100}
+          className="w-10 h-10 object-contain mb-4"
+        />
+
+        <div className="size-7 ">
+          <ShoppingIcon />
+        </div>
+      </header>
+      <h2 className="text-2xl font-medium hidden md:block">Choose category</h2>
       <Input />
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
