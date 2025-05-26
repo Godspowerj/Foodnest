a876eddd590966575a4e488a841f6684579e31b0
godspower <godspjona@gmail.com>
2025-05-27 00:48:20 +0100

    in progress

--- src/components/Aside.tsx
+++ src/components/Aside.tsx
@@ -8,10 +8,21 @@ const Aside: React.FC = () => {
   const { isCartOpen } = useContext(AppContext)!;
   return (
     <>
+      {isCartOpen && (
+        <div
+          className="fixed inset-0 bg-black/30  z-40 transition-opacity duration-300"
+          aria-hidden="true"
+        />
+      )}
+
       <div
-        className={`bg-white absolute lg:static lg:h-screen  top-0 w-full right-0 h-screen p-4 space-y-6 overflow-y-scroll scrollbar-hide rounded-t-md inset-0 transition-transform duration-500 ${
-          isCartOpen ? "translate-y-[90px]" : "translate-y-full lg:translate-y-0"
-        } `}
+        className={`bg-white fixed lg:static top-0 right-0 w-full lg:w-96 h-screen p-4 space-y-6 overflow-y-scroll scrollbar-hide rounded-t-md transition-transform duration-500 z-50
+    ${
+      isCartOpen
+        ? "translate-y-[90px] lg:translate-y-full"
+        : "translate-y-full lg:translate-y-0"
+    }
+  `}
       >
         <CartItems />
       </div>
