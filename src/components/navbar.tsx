9bd590ce07d1a358c4f31cd0797333f4053d65fa
godspower <godspjona@gmail.com>
2025-05-31 23:53:24 +0100

    in progress

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -13,30 +13,31 @@ const Navbar = () => {
     <div className="">
       <div className="lg:hidden flex items-center justify-between w-full pb-2">
         <div
-          className="size-8 cursor-pointer text-white  duration-500 transition-transform bg-[#6e4231] rounded-full flex items-center justify-center"
+          className="size-14 cursor-pointer text-black/100"
           onClick={toggleSidebar}
         >
-          {isSidebarOpen ? <CancelIcon /> : <HamburgerIcon />}
-        </div>
-
-        <div className="flex flex-col items-center">
-          <Image
-            src="/logo.png"
-            alt="Logo"
-            width={100}
-            height={100}
-            className="w-10 h-10 object-contain "
-          />
-          <h1 className="text-sm font-bold">FoodNest</h1>
+          {isSidebarOpen ? (
+            <span className="w-10 h-10 inline-block"><CancelIcon /></span>
+          ) : (
+            <span className="w-10 h-10 inline-block"><HamburgerIcon /></span>
+          )}
         </div>
 
         <div
-          className="size-8 bg-[#6e4231] text-white rounded-full flex items-center justify-center cursor-pointer"
+          className="size-10   text-red-600 bg-white shadow-sm  rounded-md flex items-center justify-center cursor-pointer"
           onClick={toggleCart}
         >
           <ShoppingIcon />
         </div>
       </div>
+
+      <div className="mb-4 ">
+        <span className="text-lg font-semibold  py-2 ">
+          Hello there!
+        </span>
+        <p className="text-gray-500 text-sm mt-1">Welcome to FoodNest. Find your favorite meals!</p>
+      </div>
+
       <div className="lg:flex  lg:items-center  w-full lg:justify-between">
         <h2 className="text-2xl font-medium hidden md:block">
           Choose category
