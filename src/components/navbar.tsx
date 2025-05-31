ef976dd65dbb598b703cfaa542bb46c9dd75044f
godspower <godspjona@gmail.com>
2025-06-01 00:53:07 +0100

    in progress

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -31,18 +31,20 @@ const Navbar = () => {
         </div>
 
         <div
-          className="size-8 md:size-10 text-red-600 bg-white shadow-sm rounded-md flex items-center justify-center cursor-pointer"
+          className="relative size-8 md:size-10 text-red-600 bg-white shadow-sm rounded-md flex items-center justify-center cursor-pointer"
           onClick={toggleCart}
         >
           <span className="w-5 h-5 md:w-6 md:h-6 flex items-center justify-center">
             <ShoppingIcon />
           </span>
+            <span className="absolute -top-2 -right-3 bg-red-600 text-white text-xs font-bold rounded-full px-1.5 py-0.5 flex items-center justify-end">0</span>
+           
         </div>
       </div>
 
       {/* Greeting Section */}
       <div className="mb-1 lg:hidden md:mb-3">
-        <span className="text-lg md:text-2xl font-semibold py-1">
+        <span className="text-2xl md:text-2xl font-semibold py-1">
           Hello there!
         </span>
         <p className="text-gray-500 text-sm md:text-lg mt-1">
