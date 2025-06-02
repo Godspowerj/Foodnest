9a12db455a846e79748b1ded9482e4d0230de28b
godspower <godspjona@gmail.com>
2025-06-02 14:14:26 +0100

    in progress

--- src/app/order/page.tsx
+++ src/app/order/page.tsx
@@ -3,9 +3,10 @@ import Navbar from "@/components/navbar";
 import React from "react";
 import { useContext } from "react";
 import { AppContext } from "@/components/AppContextApi/AppContext";
+import { CancelIcon } from "@/asset/asset";
 
 const OrderPage: React.FC = () => {
-  const { placeOrderData, placeOrder } = useContext(AppContext)!;
+  const { placeOrder } = useContext(AppContext)!;
 
   return (
     <div className="bg-[#f9f9fb] h-screen overflow-y-scroll scrollbar-hide w-full px-4 py-5">
@@ -28,23 +29,50 @@ const OrderPage: React.FC = () => {
               </tr>
             </thead>
             <tbody>
-              {placeOrder.items.map((item) => (
-                <tr key={item.id}>
-                  <td className="px-2 py-2 flex items-center gap-3">
-                    <img
-                      src={item.image}
-                      alt={item.name}
-                      className="w-12 h-12 object-cover rounded"
-                    />
-                    <span>{item.name}</span>
-                  </td>
-
-                  <td className="text-center px-2 py-2">{item.quantity}</td>
-                  <td className="text-right px-2 py-2">
-                    ₦{item.price * item.quantity}
+              {placeOrder.items.length === 0 ? (
+                <tr>
+                  <td colSpan={3} className="text-center py-4 text-gray-500">
+                    You haven't placed any order yet. Start adding delicious
+                    meals to your cart!
                   </td>
                 </tr>
-              ))}
+              ) : (
+                placeOrder.items.map((item) => (
+                  <tr key={item.id}>
+                    <td className="px-2 py-2 flex items-center gap-3">
+                      <img
+                        src={item.image}
+                        alt={item.name}
+                        className="w-12 h-12 object-cover rounded"
+                      />
+                      <div className="inline-flex items-center justify-between w-full">
+                        <span>{item.name.slice(0,12)}...</span>
+                        
+                        <button className="text-sm ">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="size-4 text-red-600 hover:text-red-800 transition"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                            />
+                          </svg>
+                        </button>
+                      </div>
+                    </td>
+                    <td className="text-center px-2 py-2">{item.quantity}</td>
+                    <td className="text-right px-2 py-2">
+                      ₦{item.price * item.quantity}
+                    </td>
+                  </tr>
+                ))
+              )}
             </tbody>
 
             <tfoot>
